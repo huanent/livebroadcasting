2d6b4bb82716f87086cd1a81cf7c6e4fa012f714
jongwong <jongwong@aliyun.com>
2020-04-09 16:15:59 +0800

    fix vuex store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueI18n from "vue-i18n";
 import router from "./router";
-
+import { store } from "src/store/index.js";
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
@@ -18,5 +18,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   i18n,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import account from './account'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
 
-export default new Vuex.Store({
-  modules: {
-    account
-  }
-})
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
