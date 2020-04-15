906b3c9f29319a0b79f6c9a626605e9c28f28d5c
laigengsen <1163455423@qq.com>
2020-04-15 18:40:57 +0800

    staged save

---
+++ src/components/common/Toolbar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="toolbar">
+    <svg icon-class="shape" :size="24" color="#333333" />
+    <svg-icon icon-class="text" :size="24" color="red" />
+    <svg-icon icon-class="laserPen" :size="24" />
+    <svg-icon icon-class="eraser" :size="24" />
+    <svg-icon icon-class="revoke" :size="24" />
+    <svg-icon icon-class="recovery" :size="24" />
+    <svg-icon icon-class="empty" :size="24" />
+    <svg-icon icon-class="newBuild" :size="24" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toolbar",
+  data() {
+    return {};
+  },
+  components: {
+    // ChatRoom,
+  },
+};
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,16 +1,21 @@
 <template>
-  <chat-room />
+  <div>
+    <toolbar />
+    <svg-icon icon-class="stop" :size="24" />
+  </div>
 </template>
 
 <script>
-import ChatRoom from "@c/liveBroadcast/ChatRoom";
+// import ChatRoom from "@c/liveBroadcast/ChatRoom";
+import Toolbar from "@c/common/Toolbar";
 export default {
   name: "Main",
   data() {
     return {};
   },
   components: {
-    ChatRoom,
+    // ChatRoom,
+    Toolbar,
   },
 };
 </script>
