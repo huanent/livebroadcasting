c53b9802b1c7449a1cdb1e4b737ef873f881830b
Jinrui Chen <caffreygo@163.com>
2020-04-15 18:13:28 +0800

    normalize css update

---
+++ src/components/liveBroadcast/ChatRoom/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="chatroom-container">
+    <div class="chatroom-body">
+      <ul class="chatroom-msg-list">
+        <li class="chatroom-msg own-msg">
+          <div class="chatroom-msg__hd">
+            <i
+              class="tic-icon tic-icon-custom"
+              style='background-image: url("https://main.qcloudimg.com/raw/92352bcabfc5fc4bc5e867d63c93c74c.jpg");'
+            ></i>
+            <div class="chatroom-msg_user">
+              <span class="chatroom-msg--name">123</span
+              ><span class="chatroom-msg--time">(17:33:52)</span>
+            </div>
+          </div>
+          <div class="chatroom-msg__bd">
+            <div class="tic-bubble">
+              <div class="tic-bubble-inner">
+                1212
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="chatroom-msg">
+          <div class="chatroom-msg__tips">
+            <span class="chatroom-msg--tips">17:30:19 123加入课堂</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="chatroom-footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatRoom",
+};
+</script>
+
+<style lang="scss" scoped>
+.chatroom-container {
+  background-color: $chatroom-bg;
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,16 @@
 <template>
-  <svg-icon icon-class="stop" :size="24" />
+  <chat-room />
 </template>
 
 <script>
+import ChatRoom from "@c/liveBroadcast/ChatRoom";
 export default {
   name: "Main",
   data() {
     return {};
+  },
+  components: {
+    ChatRoom,
   },
 };
 </script>
