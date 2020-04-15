d1d45db964118b267f07ae83a17389e806a85a8b
Jinrui Chen <caffreygo@163.com>
2020-04-15 16:55:14 +0800

    improve dir

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from "./plugins/i18n";
+import "@/plugins/icons";
 import element from "./components/common/Element/index";
 import "./assets/style/element-variable.scss";
 import "babel-polyfill";
-import "@/icons";
 
 Vue.use(element);
 
--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -6,5 +6,5 @@ Vue.component("svg-icon", SvgIcon);
 
 const requireAll = (requireContext) =>
   requireContext.keys().map(requireContext);
-const req = require.context("./svg", false, /\.svg$/);
+const req = require.context("../assets/icons", false, /\.svg$/);
 requireAll(req);
