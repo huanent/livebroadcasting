0a0807beb0c893f549733c28dd76d897ecbf1aa3
jongwong <277061456@qq.com>
2020-04-16 15:49:05 +0800

    fixed panel can not scroll

--- src/views/workplace/workplace-panel-page.vue
+++ src/views/workplace/workplace-panel-page.vue
@@ -3,13 +3,15 @@
     <div class="workplace-panel-header"></div>
     <div class="workplace-panel-content">
       <div id="workplace-panel-left">
-        <div class="camera—pannel">
-
-
-        </div>
+        <div class="camera—pannel"></div>
         <div id="gutter1-1" class="gutter-row"></div>
         <div class="main—workplace-pannel">
-          <icon name="indent" :size="24" class="test" style="padding: 0.25rem;transform:rotate(180deg)"/>
+          <icon
+            name="indent"
+            :size="24"
+            class="test"
+            style="padding: 0.25rem;transform:rotate(180deg)"
+          />
         </div>
       </div>
       <div id="gutter" class="gutter-col"></div>
@@ -17,7 +19,7 @@
         <div class="self-camera—pannel"></div>
         <div id="gutter2-1" class="gutter-row"></div>
         <div class="message—pannel">
-
+          </div>
         </div>
       </div>
     </div>
@@ -26,6 +28,7 @@
 
 <script>
 import Split from "split-grid";
+import Chatroom from "../../components/liveBroadcast/ChatRoom/index";
 export default {
   name: "workplace-panel-page",
   data: function() {
@@ -35,6 +38,7 @@ export default {
       total: 0
     };
   },
+  components: { Chatroom },
   mounted() {
     Split({
       columnGutters: [
@@ -76,7 +80,8 @@ export default {
 #workplace-panel-left {
   background: #1a1b1d;
   display: grid;
-  grid-template-rows: 20% 2px auto;
+  grid-template-rows: 20% 2px calc(80% - 2px);
+  height: calc(100vh - 2rem);
   div {
     height: 100%;
   }
@@ -84,7 +89,8 @@ export default {
 #workplace-panel-right {
   background: #212224;
   display: grid;
-  grid-template-rows: 20% 2px auto;
+  grid-template-rows: 20% 2px calc(80% - 2px);
+  height: calc(100vh - 2rem);
   div {
     height: 100%;
   }
