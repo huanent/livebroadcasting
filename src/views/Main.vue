84a12de491c2dd23e9d01a2886888e8dcf1dd9b9
Jinrui Chen <caffreygo@163.com>
2020-04-16 09:44:00 +0800

    fix conflict of Main.vue

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <chatroom />
-</template>
-
-<script>
-import Chatroom from "@c/liveBroadcast/Chatroom";
-=======
   <div>
     <toolbar />
     <svg-icon icon-class="stop" :size="24" />
@@ -15,19 +8,14 @@ import Chatroom from "@c/liveBroadcast/Chatroom";
 <script>
 // import ChatRoom from "@c/liveBroadcast/ChatRoom";
 import Toolbar from "@c/common/Toolbar";
->>>>>>> 036501d04773dbf6689d912d56263289044b60d0
 export default {
   name: "Main",
   data() {
     return {};
   },
   components: {
-<<<<<<< HEAD
-    Chatroom,
-=======
     // ChatRoom,
     Toolbar,
->>>>>>> 036501d04773dbf6689d912d56263289044b60d0
   },
 };
 </script>
