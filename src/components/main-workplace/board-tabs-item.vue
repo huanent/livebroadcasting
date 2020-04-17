caa5de42cb37848b806adaffb16aa1777870b020
jongwong <277061456@qq.com>
2020-04-17 09:37:27 +0800

    add board-tabs && cameraPannel hide handler && prettier config file

---
+++ src/components/main-workplace/board-tabs-item.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-show="isActive">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "BoardTabsItem",
+  data() {
+    return {
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/workplace/workplace-panel-page.vue
+++ src/views/workplace/workplace-panel-page.vue
@@ -2,19 +2,38 @@
   <div class="workplace-panel">
     <div class="workplace-panel-header"></div>
     <div class="workplace-panel-content">
-      <div id="workplace-panel-left">
-        <div class="camera—pannel"></div>
+      <div
+        id="workplace-panel-left"
+        :class="{ 'workplace-panel-left-hide': !showCameraPannel }"
+      >
+        <div class="camera-pannel">
+          <div
+            @click="toggleCameraPannel"
+            style="cursor: pointer;
+          height: auto;
+          position: absolute;
+          bottom: calc(-2rem - 28px)"
+          >
+            <icon
+              name="indent"
+              v-if="showCameraPannel"
+              class="pannel-icon"
+              :size="18"
+            ></icon>
+            <icon name="outdent" v-else class="pannel-icon" :size="18"></icon>
+          </div>
+        </div>
         <div id="gutter1-1" class="gutter-row"></div>
-        <div class="main—workplace-pannel">
+        <div class="main-workplace-pannel">
           <MainWorkplace></MainWorkplace>
         </div>
       </div>
       <div id="gutter" class="gutter-col"></div>
       <div id="workplace-panel-right">
-        <div class="self-camera—pannel"></div>
+        <div class="self-camera-pannel"></div>
         <div id="gutter2-1" class="gutter-row"></div>
-        <div class="message—pannel">
-          <chatroom />
+        <div class="message-pannel">
+          <!--    <chatroom />-->
         </div>
       </div>
     </div>
@@ -24,42 +43,86 @@
 <script>
 import Split from "split-grid";
 // import Toolbar from "@c/common/Toolbar";
-import MainWorkplace from "@c/main-workplace/main-workplace";
-import Chatroom from "@c/liveBroadcast/ChatRoom";
+/*import Chatroom from "@c/liveBroadcast/Chatroom/Chatroom";*/
+import MainWorkplace from "../../components/main-workplace/main-workplace";
 export default {
   name: "workplace-panel-page",
   data: function() {
     return {
       gridStyle: undefined,
+      showCameraPannel: true,
       originPosition: [0, 0],
-      total: 0,
+      total: 0
     };
   },
   components: {
-    MainWorkplace,
-    Chatroom,
+    MainWorkplace
+    /*    Chatroom,*/
   },
   mounted() {
+    const vm = this;
     Split({
       columnGutters: [
         {
           track: 1,
-          element: document.querySelector("#gutter"),
-        },
+          element: document.querySelector("#gutter")
+        }
       ],
       rowGutters: [
         {
           track: 1,
-          element: document.querySelector("#gutter1-1"),
+          element: document.querySelector("#gutter1-1")
         },
         {
           track: 1,
-          element: document.querySelector("#gutter2-1"),
-        },
+          element: document.querySelector("#gutter2-1")
+        }
       ],
+      onDragEnd: () => {
+        let str = document.getElementById("workplace-panel-left").style
+          .gridTemplateRows;
+        if (str) {
+          let list = str.trim().split(" ");
+          if (
+            list[0] &&
+            list[2] &&
+            parseFloat(list[2]) > 0 &&
+            parseFloat(list[0]) / parseFloat(list[2]) < 0.01
+          ) {
+            vm.showCameraPannel = false;
+            console.log(list[0]);
+            console.log(vm.showCameraPannel);
+          } else {
+            vm.showCameraPannel = true;
+          }
+        }
+      }
     });
   },
-  methods: {},
+  methods: {
+    toggleCameraPannel() {
+      if (!this.showCameraPannel) {
+        let str = document.getElementById("workplace-panel-left").style
+          .gridTemplateRows;
+        if (str) {
+          let list = str.trim().split(" ");
+          if (
+            list[0] &&
+            list[2] &&
+            parseFloat(list[2]) > 0 &&
+            parseFloat(list[0]) / parseFloat(list[2]) < 0.01
+          ) {
+            list[0] = "1fr";
+            list[2] = "4fr";
+            document.getElementById(
+              "workplace-panel-left"
+            ).style.gridTemplateRows = list.join(" ");
+          }
+        }
+      }
+      this.showCameraPannel = !this.showCameraPannel;
+    }
+  }
 };
 </script>
 
@@ -74,13 +137,13 @@ export default {
 }
 .workplace-panel-content {
   display: grid;
-  grid-template-columns: 79.8% 0.2% 20%;
+  grid-template-columns: 4fr 3px 1fr;
   height: 100%;
 }
 #workplace-panel-left {
   background: #1a1b1d;
   display: grid;
-  grid-template-rows: 20% 0.2% 79.8%;
+  grid-template-rows: 1fr 3px 4fr;
   height: calc(100vh - 2rem);
   div {
     height: 100%;
@@ -89,7 +152,7 @@ export default {
 #workplace-panel-right {
   background: #212224;
   display: grid;
-  grid-template-rows: 20% 0.2% 79.8%;
+  grid-template-rows: 1fr 3px 4fr;
   height: calc(100vh - 2rem);
   div {
     height: 100%;
@@ -98,23 +161,35 @@ export default {
 #gutter {
   background-color: #292929;
 }
+#gutter1-1 {
+  background-color: #292929;
+}
 .gutter-row {
   cursor: row-resize;
 }
 .gutter-col {
   cursor: col-resize;
 }
-.camera—pannel {
+.camera-pannel {
   background: #212224;
-}
-.main—workplace-pannel {
-  background: #1a1b1d;
   position: relative;
 }
-.self-camera—pannel {
+.main-workplace-pannel {
+  background: #1a1b1d;
+}
+.self-camera-pannel {
   background: #212224;
 }
-.message—pannel {
+.workplace-panel-left-hide {
+  grid-template-rows: 0 3px 1fr !important;
+}
+.message-pannel {
   background: #292b2e;
+}
+.pannel-icon {
+  fill: #8a8a8a;
+  padding: 5px;
+  background-color: rgba(0, 0, 0, 0.59);
+  transform: rotate(-90deg);
 }
 </style>
