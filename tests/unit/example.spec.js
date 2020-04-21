7830b86f731cb6616bf174a4dc85b87919fad4cb
Jinrui Chen <caffreygo@163.com>
2020-04-21 16:45:47 +0800

    add vue test util(jest) demo successful

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,15 @@
+import { mount } from "@vue/test-utils";
+import ChatroomMessage from "@/components/live-broadcast/chatroom/chatroom-message.vue";
+
+describe("ChatroomMessage.vue组件", () => {
+  it("测试ChatroomMessage组件能否正常显示并增加", () => {
+    const wrapper = mount(ChatroomMessage, {
+      methods: {
+        $t: () => ""
+      }
+    });
+    // expect(wrapper.find('p').text()).toBe(0);
+    // wrapper.find('button').trigger('click');
+    // expect(wrapper.find('p').text()).toBe(1);
+  });
+});
