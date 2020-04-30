dfeb49460786a73e68e9010b2b2c285dedf99cf1
李凤焕 <huanent@icloud.com>
2020-04-30 14:43:51 +0800

    improved eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,16 @@ module.exports = {
     node: true
   },
 
-  extends: ["plugin:vue/essential", "eslint:recommended"],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
 
   parserOptions: {
     parser: "babel-eslint"
   },
 
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
-    "no-unused-vars": "off"
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
 
   overrides: [
