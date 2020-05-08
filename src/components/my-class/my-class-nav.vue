d3c8904d3fe7334779b3b0c4d6f58ff9097d128a
Jinrui Chen <caffreygo@163.com>
2020-05-08 16:12:54 +0800

    feat: style of my-class-nav

---
+++ src/components/my-class/my-class-nav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="my-class-nav">
+    <div class="my-class-wrap">
+      <div class="logo-wrap">Live broadcast</div>
+      <div class="serach-wrap">
+        <el-input v-model="value" suffix-icon="el-icon-search"></el-input>
+      </div>
+      <div>我的课堂</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyClassNav",
+  data() {
+    return {
+      value: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.my-class-nav {
+  height: 60px;
+  color: $white;
+  font-size: 14px;
+  background-color: #2e333f;
+  .my-class-wrap {
+    width: 1205px;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .logo-wrap {
+      font-size: 32px;
+    }
+    .serach-wrap {
+      width: 320px;
+    }
+  }
+}
+</style>
--- src/views/myclass/index.vue
+++ src/views/myclass/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <h1>MyClass</h1>
+  <div>
+    <my-class-nav />
+    <div>myclass-head</div>
+    <div>myclass-main</div>
+  </div>
 </template>
 
 <script>
+import MyClassNav from "@c/my-class/my-class-nav";
+
 export default {
   name: "MyClass",
   data() {
     return {};
+  },
+  components: {
+    MyClassNav
   }
 };
 </script>
