1b6f3019fdd442ed1588bdfde3cdafd44369df9a
李凤焕 <huanent@icloud.com>
2020-05-12 16:09:32 +0800

    add clicker widget

---
+++ src/components/live-broadcast/widgets/clicker-widget/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <widget-window class="clicker">
+    <question />
+  </widget-window>
+</template>
+<script>
+import WidgetWindow from "../widget-window";
+import Question from "./question";
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  components: {
+    WidgetWindow,
+    Question
+  }
+};
+</script>
+<style lang="scss" scoped>
+.clicker {
+  width: 400px;
+}
+</style>
---
+++ src/components/live-broadcast/widgets/clicker-widget/question.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-input type="textarea" :rows="2" placeholder="题目" v-model="title" />
+    <div>
+      <question-item
+        v-for="(item, index) in items"
+        :key="index"
+        :label="labels[index]"
+        :detail.sync="item.detail"
+        :is-true.sync="item.isTrue"
+        :erasable="items.length > 2"
+        @remove="remove(item)"
+      />
+    </div>
+    <el-button type="info" round size="mini" @click="add">+</el-button>
+    <el-button class="start-btn" type="primary" size="mini">开始答题</el-button>
+  </div>
+</template>
+
+<script>
+import QuestionItem from "./question-item";
+
+export default {
+  data() {
+    return {
+      labels: ["A", "B", "C", "D", "E", "F", "G", "H"],
+      title: "4是质数吗？",
+      items: [
+        { detail: "是", isTrue: true },
+        { detail: "否", isTrue: false }
+      ]
+    };
+  },
+  methods: {
+    add() {
+      this.items.push({ detail: "", isTrue: false });
+    },
+    remove(item) {
+      this.items = this.items.filter(f => f != item);
+    }
+  },
+  components: {
+    QuestionItem
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.start-btn {
+  width: 100%;
+  border-radius: 0;
+  margin: 0;
+}
+</style>
