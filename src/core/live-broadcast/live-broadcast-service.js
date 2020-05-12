666a3edc36000bb8fd42c28edbd391612e10a6e0
jongwong <277061456@qq.com>
2020-05-12 09:21:19 +0800

    add ws pushState

--- src/core/live-broadcast/live-broadcast-service.js
+++ src/core/live-broadcast/live-broadcast-service.js
@@ -2,17 +2,19 @@ import { createRoom, enterRoom } from "../data/data-service";
 import store from "@/store";
 import COS from "cos-js-sdk-v5";
 window["COS"] = COS;
-
+import Vue from "vue";
 import { Emitter } from "../emit";
 import { TrtcService } from "./trtc-service";
 import { TimService } from "./tim-service";
 import { BoardService } from "./board-service";
+import { app } from "../../main";
+import { pushState } from "./tim-message/send";
 
 class LiveBroadcastService {
   config;
   mode = "live";
   TokenList = {};
-  roomId = "113736542811385";
+  roomId = "113737064261488";
   activeBoard = null;
   userId = "jinrui";
   tim;
@@ -50,10 +52,15 @@ class LiveBroadcastService {
       this.trtcService.init(this.roomId, this);
       await this.boardService.init(this.roomId, this);
       await this.timService.listenHandler();
+      observerJson = getDataSate();
+      watchState();
       return true;
     } else {
       console.error(res.data.messages);
     }
+  }
+  async destroy() {
+    unwWatchState();
   }
 }
 
@@ -64,3 +71,54 @@ Emitter.on("LIVE_INIT", async () => {
   await liveBroadcastService.init();
   Emitter.emit("LIVE_READY");
 });
+const props = ["boardTotalPage", "boardNumber", "boardScale", "panelType"];
+
+const getDataSate = function() {
+  let ob = {};
+  props.forEach(key => {
+    if (store.state.workplace[key]) {
+      ob[key] = store.state.workplace[key];
+    }
+  });
+  return { workplace: ob };
+};
+const getDiff = function(origin, target) {
+  let ob;
+  ob = {};
+  for (let i in origin) {
+    try {
+      if (JSON.stringify(target[i]) !== JSON.stringify(origin[i])) {
+        ob[i] = origin[i];
+      }
+    } catch (e) {
+      ob[i] = origin[i];
+    }
+  }
+  return ob;
+};
+let watchInstance = {};
+let observerJson = {};
+
+const watchState = function() {
+  props.forEach(key => {
+    if (store.state.workplace[key]) {
+      watchInstance[key] = app.$watch("$store.state.workplace." + key, {
+        deep: true,
+        handler: async function(a, b) {
+          let oldVal = observerJson;
+          observerJson = getDataSate();
+          let diff = getDiff(observerJson.workplace, oldVal.workplace);
+          await pushState(diff);
+        }
+      });
+    }
+  });
+};
+
+const unwWatchState = function() {
+  props.forEach(key => {
+    if (store.state.workplace[key]) {
+      watchInstance[key]();
+    }
+  });
+};
