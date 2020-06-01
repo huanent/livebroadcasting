040ef3654ded3fbb0d730ff5c052fb78e14b930e
李凤焕 <huanent@icloud.com>
2020-06-01 17:55:34 +0800

    improved code

--- src/core/utils.js
+++ src/core/utils.js
@@ -5,24 +5,26 @@ export const delay = async time =>
   new Promise(rs => setTimeout(_ => rs(), time));
 
 export const requestDeviceAccess = async function() {
-  let video;
-  let audio;
-
-  try {
-    video = await navigator.mediaDevices.getUserMedia({
-      video: true
-    });
-  } catch (e) {
-    console.log(e);
-  }
-
-  try {
-    audio = await navigator.mediaDevices.getUserMedia({
-      audio: true
-    });
-  } catch (e) {
-    console.log(e);
-  }
-
+  let video = await getMediaStream("video");
+  let audio = await getMediaStream("video");
   return { video, audio };
 };
+
+async function getMediaStream(type) {
+  try {
+    let stream = await navigator.mediaDevices.getUserMedia({
+      video: type == "video",
+      audio: type == "audio"
+    });
+
+    var tracks = stream.getTracks();
+
+    for (const i of tracks) {
+      i.stop();
+    }
+
+    return stream;
+  } catch (e) {
+    console.log(e);
+  }
+}
