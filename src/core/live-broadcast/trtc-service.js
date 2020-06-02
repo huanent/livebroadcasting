1d516ea5462b9777fefd8f657ca5ec7d36fa0f32
李凤焕 <huanent@icloud.com>
2020-06-02 18:25:27 +0800

    temp

--- src/core/live-broadcast/trtc-service.js
+++ src/core/live-broadcast/trtc-service.js
@@ -170,7 +170,7 @@ export class TrtcService {
       video: !!access.video
     });
 
-    this.localStream.setVideoProfile(this.isTeacher ? "720p" : "480p");
+    this.localStream.setVideoProfile(this.isTeacher ? "720p" : "240p");
 
     try {
       await this.localStream.initialize();
