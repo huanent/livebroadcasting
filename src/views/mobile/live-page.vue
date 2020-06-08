b4f83f06fa890e64887ad3245db1cb484a439144
Jinrui Chen <caffreygo@163.com>
2020-06-08 17:24:21 +0800

    layout of mobile live page

---
+++ src/views/mobile/live-page.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="m-liveroom">
+    <div class="m-sidebar"></div>
+    <div class="m-main">
+      <div class="m-live-area"></div>
+      <div class="m-control"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LivePage",
+  data: () => ({
+    title: "hello"
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+.m-liveroom {
+  height: 100%;
+  overflow: hidden;
+  .m-sidebar {
+    float: left;
+    width: 10rem;
+    height: 100%;
+    background-color: aquamarine;
+  }
+  .m-main {
+    float: left;
+    position: relative;
+    height: 100%;
+    width: calc(100% - 10rem);
+    .m-live-area {
+      height: 100%;
+      width: 100%;
+      background-color: aliceblue;
+    }
+    .m-control {
+      width: 5em;
+      height: 10rem;
+      background-color: cadetblue;
+      position: absolute;
+      right: 0;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+}
+</style>
