b6801bcfc2c05ba65fcae665bd85ef7f59c09436
jongwong <277061456@qq.com>
2020-08-03 11:40:45 +0800

    fixed dice

--- src/views/LivePanel/components/widgets/DiceWidget/index.vue
+++ src/views/LivePanel/components/widgets/DiceWidget/index.vue
@@ -5,92 +5,56 @@
     :position="dice.position"
     v-if="dice.visible"
     @moved="UPDATE_POSITION({ name: 'dice', position: $event })"
-    :class="{ 'mobile-scale': isMobile }"
   >
-    <transition>
-      <div class="dice">
+    <div class="contain">
+      <div class="dice" ref="dice" @click="start">
         <div
-          id="content"
-          ref="dice"
-          @click="start"
-          :class="point"
+          class="side"
+          v-for="(group, key, index) in pointerList"
+          :key="index"
+          v-if="key === pointer"
         >
-          <!-- 第一个筛子 -->
-          <div>
-            <span></span>
-          </div>
-
-          <!-- 第二个筛子 -->
-          <div>
-            <span></span>
-            <span></span>
-          </div>
-
-          <!-- 第三个筛子 -->
-          <div>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-
-          <!-- 第四个 -->
-
-          <div>
-            <div>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <!-- 第五个 -->
-          <div>
-            <div>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-
-          <!-- 第六个-->
-          <div>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
+          <div class="pointer-group" v-for="group in pointerList[key]">
+            <div class="pointer-wrapper" v-for="p in group">
+              <div
+                class="pointer"
+                :style="{
+                  'background-color': p.color && p.color,
+                  height: p.size ? p.size + 'px' : '22px',
+                  width: p.size ? p.size + 'px' : '22px',
+                  'border-radius': p.size ? p.size / 2 + 'px' : '11px'
+                }"
+              ></div>
             </div>
           </div>
         </div>
       </div>
-    </transition>
+    </div>
   </widget-window>
 </template>
 <script>
 import WidgetWindow from "../WidgetWindow";
 import { mapState, mapMutations, mapGetters } from "vuex";
+import BezierAnimation from "../../../../../core/common/animation/animation";
+var vm;
 export default {
   name: "DiceWidget",
   components: {
     WidgetWindow
   },
   data() {
+    vm = this;
     return {
-      point: ""
+      pointer: "1",
+      pointerList: {},
+      animation: undefined
     };
   },
+  mounted() {
+    this.initPointerList();
+    this.pointer = this.dice.value + "";
+  },
+
   computed: {
     ...mapState("widget", ["dice"]),
     ...mapState("device", ["isMobile"])
@@ -102,208 +66,125 @@ export default {
       "SET_DICE_VISIBLE",
       "SET_DICE_START"
     ]),
+    getSize(p) {
+      if (p.size) return p.size + "rpx";
+      return "";
+    },
+    initPointerList() {
+      let red = "#b10501";
+      let blue = "#1021b7";
+      this.pointerList = {
+        1: [[{ name: "" }, { name: "1", color: red, size: 27 }, { name: "" }]],
+        2: [
+          [{ name: "1", color: blue }, { name: "" }, { name: "2", color: blue }]
+        ],
+        3: [
+          [{ name: "1", color: blue }, { name: "" }, { name: "" }],
+          [{ name: "" }, { name: "2", color: blue }, { name: "" }],
+          [{ name: "" }, { name: "" }, { name: "3", color: blue }]
+        ],
+        4: [
+          [
+            { name: "1", color: red },
+            { name: "2", color: red }
+          ],
+          [
+            { name: "3", color: red },
+            { name: "4", color: red }
+          ]
+        ],
+        5: [
+          [
+            { name: "1", color: blue },
+            { name: "" },
+            { name: "2", color: blue }
+          ],
+          [{ name: "" }, { name: "3", color: blue }, { name: "" }],
+          [{ name: "4", color: blue }, { name: "" }, { name: "5", color: blue }]
+        ],
+        6: [
+          [
+            { name: "1", color: blue },
+            { name: "" },
+            { name: "2", color: blue }
+          ],
+          [
+            { name: "3", color: blue },
+            { name: "" },
+            { name: "4", color: blue }
+          ],
+          [{ name: "5", color: blue }, { name: "" }, { name: "6", color: blue }]
+        ]
+      };
+    },
     start() {
       let num = parseInt(1 + Math.random() * 6);
       this.SET_DICE_VALUE(num);
 
       this.SET_DICE_START(true);
-      setTimeout(()=> {
+      setTimeout(() => {
         this.SET_DICE_START(false);
-      },2000)
+      }, 2000);
     }
   },
   watch: {
-    "dice.value"(value) {
-      this.point = "";
-      if (value == 1) this.point = "pointone";
-      else if (value == 2) this.point = "pointtwo";
-      else if (value == 3) this.point = "pointthree";
-      else if (value == 4) this.point = "pointfour";
-      else if (value == 5) this.point = "pointfive";
-      else if (value == 6) this.point = "pointsix";
+    "dice.start": function() {
+      this.animation = new BezierAnimation(2, "ease", (i1, i2) => {
+        let num = parseInt(1 + Math.random() * 6);
+
+        if (i2 >= 1) {
+          num = this.dice.value;
+        }
+        this.pointer = num + "";
+      });
+      this.animation.play();
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-.dice {
+.contain {
   width: 210px;
   height: 200px;
   background: #54555d;
-  padding-left: 36px;
+  position: relative;
   box-sizing: border-box;
-  padding-top: 28px;
-  #content {
-    width: 134px;
-    height: 134px;
-    transform-style: preserve-3d;
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  #content > div {
-    width: 100px;
-    height: 100px;
-    padding: 15px;
-    display: flex;
-    background: #fff;
-    border-radius: 15px;
-    position: absolute;
-    /* box-shadow: 2px 1px 5px #333; */
-    border: 2px solid #eee;
-  }
-
-  #content span {
-    width: 25px;
-    height: 25px;
-    display: block;
-    border-radius: 50%;
-    background: #c33;
-  }
-
-  /* 1 */
-  #content > div:nth-child(1) {
-    justify-content: center;
-    align-items: center;
-    transform: translateZ(65px);
-  }
-
-  /* 2 */
-  #content > div:nth-child(2) {
-    justify-content: space-between;
-    align-items: center;
-    transform: translateZ(-65px);
-  }
-
-  /* 3 */
-  #content > div:nth-child(3) {
-    justify-content: space-between;
-    transform: rotateX(90deg) translateZ(65px);
-  }
-
-  #content > div:nth-child(3) span:nth-child(2) {
-    align-self: center;
-  }
-
-  #content > div:nth-child(3) span:nth-child(3) {
-    align-self: flex-end;
-  }
-
-  /* 4 */
-  #content > div:nth-child(4) {
-    transform: rotateX(90deg) translateZ(-65px);
-    justify-content: space-between;
-  }
-
-  #content > div:nth-child(4) div {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  /* 5 */
-  #content > div:nth-child(5) {
-    transform: rotateY(90deg) translateZ(65px);
-    justify-content: space-between;
-  }
-
-  #content > div:nth-child(5) div {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  #content > div:nth-child(5) div:nth-child(2) {
-    justify-content: center;
-  }
-
-  /* 6 */
-  #content > div:nth-child(6) {
-    transform: rotateY(90deg) translateZ(-65px);
-    justify-content: space-between;
-  }
-
-  #content > div:nth-child(6) div {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
+  display: flex;
 }
 
-@keyframes move1 {
-  0% {
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  100% {
-    transform: rotateY(3600deg) rotateX(3600deg);
-  }
+.dice {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.side {
+  width: 90px;
+  height: 90px;
+  padding: 5px;
+  display: flex;
+  background: #fff;
+  border-radius: 10px;
+  flex-direction: column;
 }
 
-@keyframes move2 {
-  0% {
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  100% {
-    transform: rotateY(3510deg) rotateX(3600deg);
-  }
+.pointer-group {
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+  flex-direction: row;
+  align-items: center;
 }
-
-@keyframes move3 {
-  0% {
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  100% {
-    transform: rotateY(3690deg) rotateX(3600deg);
-  }
+.pointer {
+  text-align: center;
+  /*width: 16px;*/
+  /*height: 16px;*/
+  /*background-color: #c33;*/
 }
-
-@keyframes move4 {
-  0% {
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  100% {
-    transform: rotateY(3780deg) rotateX(3600deg);
-  }
-}
-
-@keyframes move5 {
-  0% {
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  100% {
-    transform: rotateY(3600deg) rotateX(3690deg);
-  }
-}
-
-@keyframes move6 {
-  0% {
-    transform: rotateY(30deg) rotateX(-20deg);
-  }
-
-  100% {
-    transform: rotateY(3600deg) rotateX(3510deg);
-  }
-}
-.pointone {
-  animation: move1 2s ease-in-out forwards;
-}
-.pointtwo {
-  animation: move4 2s ease-in-out forwards;
-}
-.pointthree {
-  animation: move6 2s ease-in-out forwards;
-}
-.pointfour {
-  animation: move5 2s ease-in-out forwards;
-}
-.pointfive {
-  animation: move2 2s ease-in-out forwards;
-}
-.pointsix {
-  animation: move3 2s ease-in-out forwards;
+.pointer-wrapper {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 }
 </style>
